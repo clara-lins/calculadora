dd1394972f0ecd37bb749bc10b861d40f5704db7
Clara Lins <claralousado03@gmail.com>
2021-02-22 19:28:28 -0300

    Calculadora em python com soma funcionando

---
+++ calculadora.py
@@ -0,0 +1,16 @@
+def soma(a,b):
+    return a+b
+    print("Oi")
+
+
+def sub(a,b):
+    return a-b
+
+
+def main():
+    a= int(input("Digite um número: "))
+    b= int(input("Digite um número: "))
+    resultado = soma(a,b)
+    print("Resultado eh: ", resultado)
+
+main()
